868e5510101a78c08486bda63a2fb3015f9e4b6b
TanishqLambhate <tanulambhate@gmail.com>
2023-03-07 00:33:13 +0530

    done vercel setting3

--- ecomsite/urls.py
+++ ecomsite/urls.py
@@ -18,7 +18,8 @@ from django.urls import path
 from shop import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index,name='index'),
+    path(r'^$',views.index,name='index'),
     path('<int:id>/',views.detail,name='detail'),
     path('checkout/',views.checkout,name='checkout'),
+    
 ]
