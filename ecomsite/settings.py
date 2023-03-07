713a67c32e78b7e7cf9cd6b43199760c93873ebf
TanishqLambhate <tanulambhate@gmail.com>
2023-03-07 17:20:00 +0530

    done vercel setting4

--- ecomsite/settings.py
+++ ecomsite/settings.py
@@ -75,10 +75,10 @@ WSGI_APPLICATION = 'ecomsite.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
@@ -120,5 +120,7 @@ USE_TZ = True
 
 import os
 STATIC_URL = '/static/'
-STATICFILES_DIRS =os.path.join(BASE_DIR,'static'),
-STATIC_ROOT =os.path.join(BASE_DIR,'staticfiles_build','static')
+# STATICFILES_DIRS =os.path.join(BASE_DIR,'static'),
+# STATIC_ROOT =os.path.join(BASE_DIR,'staticfiles_build','static')
+STATICFILES_DIRS = [BASE_DIR/'static',]
+STATIC_ROOT = BASE_DIR/'staticfiles'
